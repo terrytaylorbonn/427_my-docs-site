b4864580d9aa3d4818a70218cfde71fd9b72e208
terrytaylorbonn <terrytaylorbonn@gmail.com>
2025-07-13 05:58:23 -0400

    TT68 google analytics ready for deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,209 @@ module.exports = {
         content: 'FF14F5E36F82A306',
       },
     },
+    // (1) Open Graph meta tags #############
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:title',
+        content: 'My Docs Site - Technical documentation made simple',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:description',
+        content: 'Comprehensive technical documentation and tutorials. Learn through clear guides and examples.',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:image',
+        content: 'https://four27-my-docs-site.onrender.com/img/docusaurus-social-card.jpg',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:url',
+        content: 'https://four27-my-docs-site.onrender.com',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:type',
+        content: 'website',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:site_name',
+        content: 'My Docs Site',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:locale',
+        content: 'en_US',
+      },
+    },
+    // #############
+    // (2) Twitter Card meta tags #############
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:title',
+        content: 'My Docs Site - Technical documentation made simple',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:description',
+        content: 'Comprehensive technical documentation and tutorials. Learn through clear guides and examples.',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:image',
+        content: 'https://four27-my-docs-site.onrender.com/img/docusaurus-social-card.jpg',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:site',
+        content: '@mydocssite',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:creator',
+        content: '@terrytaylorbonn',
+      },
+    },
+    // #############
+    // (3) Structured Data (JSON-LD) for search engines #############
+    {
+      tagName: 'script',
+      attributes: {
+        type: 'application/ld+json',
+      },
+      innerHTML: JSON.stringify({
+        '@context': 'https://schema.org',
+        '@graph': [
+          {
+            '@type': 'Organization',
+            '@id': 'https://four27-my-docs-site.onrender.com/#organization',
+            name: 'My Docs Site',
+            url: 'https://four27-my-docs-site.onrender.com',
+            logo: {
+              '@type': 'ImageObject',
+              url: 'https://four27-my-docs-site.onrender.com/img/logo.svg'
+            },
+            sameAs: [
+              'https://github.com/terrytaylorbonn/427_my-docs-site'
+            ]
+          },
+          {
+            '@type': 'WebSite',
+            '@id': 'https://four27-my-docs-site.onrender.com/#website',
+            url: 'https://four27-my-docs-site.onrender.com',
+            name: 'My Docs Site',
+            description: 'Technical documentation made simple',
+            publisher: {
+              '@id': 'https://four27-my-docs-site.onrender.com/#organization'
+            },
+            potentialAction: {
+              '@type': 'SearchAction',
+              target: 'https://four27-my-docs-site.onrender.com/search?q={search_term_string}',
+              'query-input': 'required name=search_term_string'
+            }
+          },
+          {
+            '@type': 'WebPage',
+            '@id': 'https://four27-my-docs-site.onrender.com/#webpage',
+            url: 'https://four27-my-docs-site.onrender.com',
+            name: 'My Docs Site - Technical documentation made simple',
+            description: 'Comprehensive technical documentation and tutorials. Learn through clear guides and examples.',
+            isPartOf: {
+              '@id': 'https://four27-my-docs-site.onrender.com/#website'
+            },
+            about: {
+              '@id': 'https://four27-my-docs-site.onrender.com/#organization'
+            },
+            datePublished: '2025-07-12',
+            dateModified: '2025-07-12'
+          }
+        ]
+      })
+    },
+    // #############
+    // (4) Canonical URLs and language tags #############
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'canonical',
+        href: 'https://four27-my-docs-site.onrender.com',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'alternate',
+        hreflang: 'en',
+        href: 'https://four27-my-docs-site.onrender.com',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'alternate',
+        hreflang: 'x-default',
+        href: 'https://four27-my-docs-site.onrender.com',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'language',
+        content: 'en',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        httpEquiv: 'content-language',
+        content: 'en-US',
+      },
+    },
+    // #############
   ],
-  // ##############
+
+  // (GA) Google Analytics configuration #############
+  plugins: [
+    [
+      '@docusaurus/plugin-google-gtag',
+      {
+        trackingID: 'G-MX9MT9MK97',
+        anonymizeIP: true,
+      },
+    ],
+  ],
+  // #############
 
   presets: [
     [
